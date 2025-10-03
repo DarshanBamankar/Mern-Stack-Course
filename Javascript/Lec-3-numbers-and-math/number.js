990b598bef177a79925989da18a130d9398b776e
DarshanBamankar <darshanbamankar7@gmail.com>
2025-10-03 22:43:34 +0530

    Lec 3 - numbers and math - Learnt about various methods we can perform on numbers in js and how do we get those methods. Also learnt another way to create number (as objects).

--- Javascript/Lec-3-numbers-and-math/number.js
+++ Javascript/Lec-3-numbers-and-math/number.js
@@ -1,6 +1,10 @@
 let numOne = 10.44879625;
 let numTwo = 20;
 
+// Here numOne and numTwo are plain numbers (not an object), so how do we get the methods and properties associated to it?
+
+// Because when we access the methods such as tofixed(), toPrecision(), etc -> Javascript wraps the primitive into a number object automatically -> which is called autoboxing.
+
 // Methods 
 
 // 1) tofixed(number) - to take numbers until desired decimal places
@@ -20,3 +24,16 @@ console.log(preciseNum); // will return 10.4488 as a string
 let stringNum = numOne.toString();
 console.log(stringNum);
 
+// Another method for creating a number
+
+let newNum = new Number(50); // creates a new number (50) as a object
+let newNumTwo = new Number(50); // creates a new number (50) as a object
+
+console.log(newNum == newNumTwo); // returns false
+
+// Because both the numbers are created as a object.
+// Objects are compared by checking if they are pointing to the same memory location.
+// In this case newNum and newNumTwo are pointing to different memory locations.
+// If we do newNum = newNumTwo -> now it will return true, as both are pointing to same memory location.
+
+// Object is always true in boolean evem if empty, because the memory location (refernce) is present even if there is no value. 
