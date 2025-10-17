b425669eb9fc2a8440bbed97fdf117924c0b64e4
DarshanBamankar <darshanbamankar7@gmail.com>
2025-10-17 20:07:40 +0530

    Learnt basics of objects in JS.

---
+++ Javascript/lec-6-objects/obj.js
@@ -0,0 +1,60 @@
+/* Objects store information in key-value pairs. */
+
+/* The keys are stored as strings */
+
+const accountHolder = {
+    name : "Darshan",
+    age : 19,
+    email : "darshan@gmail.com",
+    accountNo : "123456789",
+    bankId : "1254"
+}
+
+console.log(accountHolder);
+
+// Accessing object values using dot operator
+
+    let username = accountHolder.name;
+    // OR
+    let alsoUsername = accountHolder["name"];
+    
+    console.log(username);
+
+// Inserting values in objects 
+
+    accountHolder.addharNo = "125487956";
+    console.log(accountHolder);
+
+    
+// Updating values in objects 
+
+    accountHolder.addharNo = "XXXXXXXX5897";
+    console.log(accountHolder);
+
+// Deleting values from a object
+
+// Using 'delete' keyword
+
+    delete accountHolder.bankId;
+    console.log(accountHolder);
+
+// Accessing the keys of a object 
+
+    console.log(Object.keys(accountHolder));
+    // returns the keys of 'accountHolder' object in a array    
+
+// Accessing the values of a object
+    
+    console.log(Object.values(accountHolder));
+    // returns the values of 'accountHolder' object in a array    
+
+// To get both (keys and values)
+
+    console.log(Object.entries(accountHolder));
+    // returns the key-value pairs in the 'accountHolder' object in a array
+
+// Looping over a Object
+
+    for (let p in accountHolder){
+        console.log(Object.entries(accountHolder));
+    }
